793af726a4648977b492dbe425d5ef1315ad0d4d
serafinovsky <m.serafinovskiy@dev.kokoc.com>
2025-03-04 11:20:57 +0700

    tls

--- services/front/src/config.ts
+++ services/front/src/config.ts
@@ -1,10 +1,10 @@
 export const COLORS = [
-  '#0369A1', // sky-700 
-  '#D97706', // amber-600
-  '#059669', // emerald-600
-  '#C026D3',  // fuchsia-600
-  '#DC2626', // red-600 
+  "#0369A1", // sky-700
+  "#D97706", // amber-600
+  "#059669", // emerald-600
+  "#C026D3", // fuchsia-600
+  "#DC2626", // red-600
 ];
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const BASE_WS_URL = import.meta.env.VITE_WS_BASE_URL;
+export const BASE_URL = "https://kingdoms-game.ru";
+export const BASE_WS_URL = "wss://kingdoms-game.ru";
